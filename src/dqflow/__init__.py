833b7a9634cc3b91c2e97b88f3a038e5bf8b3206
Quang <nguyenvanquang247@gmail.com>
2026-01-31 12:02:19 +0700

    Bump version to 0.1.3

--- src/dqflow/__init__.py
+++ src/dqflow/__init__.py
@@ -4,5 +4,5 @@ from dqflow.column import Column
 from dqflow.contract import Contract
 from dqflow.result import ValidationResult
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __all__ = ["Contract", "Column", "ValidationResult"]
